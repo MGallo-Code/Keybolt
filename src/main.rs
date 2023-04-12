d2996a51d3c61381fb756f110e9bab84a7253ab7
MGallo-Code <mgallo.code@gmail.com>
2023-04-12 13:51:57 -0400

    Added comments main + profile_page.rs

--- src/main.rs
+++ src/main.rs
@@ -3,57 +3,69 @@ use iced::pure::widget::{Button, Column, Container, Text};
 use iced::pure::Sandbox;
 use profile_page::ProfilePage;
 
+// Include each page
 mod profile_page;
 
+// Run application
 fn main() -> Result<(), iced::Error> {
-    PageView::run(Settings::default())
+    // Start the application with default settings
+    AppView::run(Settings::default())
 }
 
-struct PageView {
+// The main struct representing the application's state and its views
+struct AppView {
     current_view: Views,
-    profile_page: ProfilePage
+    profile_page: ProfilePage,
 }
 
+// An enumeration of the different views in the application
 #[derive(Debug, Clone, Copy)]
 pub enum Views {
     MainPage,
-    ProfilePage
+    ProfilePage,
 }
 
+// An enumeration of the messages the application can receive
 #[derive(Debug, Clone, Copy)]
-pub enum PageViewMsg {
+pub enum AppMsg {
     ChangePage(Views),
 }
 
-impl Sandbox for PageView {
-    type Message = PageViewMsg;
+// Implement the Sandbox trait for the AppView struct
+impl Sandbox for AppView {
+    type Message = AppMsg;
 
+    // Initialize a new AppView struct with default values
     fn new() -> Self {
-        PageView {
+        AppView {
             current_view: Views::MainPage,
-            profile_page: ProfilePage::new()
+            profile_page: ProfilePage::new(),
         }
     }
 
+    // Return the title of the application
     fn title(&self) -> String {
         String::from("Counter app")
     }
 
+    // Update the application's state based on received messages
     fn update(&mut self, message: Self::Message) {
         match message {
-            PageViewMsg::ChangePage(view) => self.current_view = view,
+            AppMsg::ChangePage(view) => self.current_view = view,
         }
     }
 
+    // Define the application's user interface layout based on its state
     fn view(&self) -> iced::pure::Element<Self::Message> {
         let label = Text::new(format!("View: {}", "Main"));
-        let profile_btn = Button::new("Profile").on_press(PageViewMsg::ChangePage(Views::ProfilePage));
+        let profile_btn = Button::new("Profile").on_press(AppMsg::ChangePage(Views::ProfilePage));
         let col = Column::new().push(label).push(profile_btn);
         let main_page_layout = Container::new(col).center_x().center_y().width(iced::Length::Fill).height(iced::Length::Fill);
 
+        // Display the appropriate view based on the current_view value
         match self.current_view {
             Views::MainPage => main_page_layout.into(),
-            Views::ProfilePage => self.profile_page.view()
+            Views::ProfilePage => self.profile_page.view(),
         }
     }
 }
--- src/profile_page.rs
+++ src/profile_page.rs
@@ -2,20 +2,32 @@ use iced::Length;
 use iced::pure::widget::{Button, Column, Container, Text};
 use iced::pure::Element;
 
+// Import the Views enum and the AppMsg enum from the main application module
 use crate::Views;
-use crate::PageViewMsg;
+use crate::AppMsg;
 
+// Define the ProfilePage struct
 pub struct ProfilePage;
 
+// Implement methods for the ProfilePage struct
 impl ProfilePage {
+    // Create a new instance of the ProfilePage struct
     pub fn new() -> Self {
         ProfilePage
     }
 
-    pub fn view(&self) -> Element<PageViewMsg> {
+    // Define the user interface layout for the ProfilePage
+    pub fn view(&self) -> Element<AppMsg> {
+        // Create a text label for the ProfilePage
         let label = Text::new("Page 2");
-        let main_menu_btn = Button::new("Main Menu").on_press(PageViewMsg::ChangePage(Views::MainPage));
+
+        // Create a button to return to the main menu
+        let main_menu_btn = Button::new("Main Menu").on_press(AppMsg::ChangePage(Views::MainPage));
+
+        // Create a column layout, add the label and button to it
         let col = Column::new().push(label).push(main_menu_btn);
+
+        // Create a container to hold the column layout, set its dimensions and position, and return it as an Element
         Container::new(col)
             .width(Length::Fill)
             .height(Length::Fill)
